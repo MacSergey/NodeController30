e00fda3d28634eb706147fda6c0a4fff0ceb4428
MacSergey <macsergey@me.com>
2021-04-30 17:24:15 +0300

    Fix icon add beta preview

--- NodeControllerRenewal/Utilities/Textures.cs
+++ NodeControllerRenewal/Utilities/Textures.cs
@@ -18,20 +18,12 @@ namespace NodeController.Utilities
         public static string IconActive => nameof(IconActive);
         public static string IconHover => nameof(IconHover);
 
-        public static string CursorEdit => nameof(CursorEdit);
-        public static string CursorError => nameof(CursorError);
-        public static string CursorInsert => nameof(CursorInsert);
-        public static string CursorCrossing => nameof(CursorCrossing);
-        public static string CursorMove => nameof(CursorMove);
-        public static string CursorSearch => nameof(CursorSearch);
-
         public static string Reset => nameof(Reset);
         public static string MakeStraight => nameof(MakeStraight);
 
         private static Dictionary<string, TextureHelper.SpriteParamsGetter> Files { get; } = new Dictionary<string, TextureHelper.SpriteParamsGetter>
         {
             {nameof(Button), Button},
-            {nameof(Cursor), Cursor},
             {nameof(HeaderButtons), HeaderButtons},
         };
 
@@ -40,8 +32,7 @@ namespace NodeController.Utilities
             Atlas = TextureHelper.CreateAtlas(nameof(NodeController), Files);
         }
 
-        private static UITextureAtlas.SpriteInfo[] Button(int texWidth, int texHeight, Rect rect) => TextureHelper.GetSpritesInfo(texWidth, texHeight, rect, 31, 31, new RectOffset(0, 0, 0, 0), 2, ButtonNormal, ButtonActive, ButtonHover, Icon, IconActive, IconHover).ToArray();
-        private static UITextureAtlas.SpriteInfo[] Cursor(int texWidth, int texHeight, Rect rect) => TextureHelper.GetSpritesInfo(texWidth, texHeight, rect, 31, 31, CursorEdit, CursorError, CursorInsert, CursorCrossing, CursorMove, CursorSearch).ToArray();
+        private static UITextureAtlas.SpriteInfo[] Button(int texWidth, int texHeight, Rect rect) => TextureHelper.GetSpritesInfo(texWidth, texHeight, rect, 31, 31, ButtonNormal, ButtonActive, ButtonHover, Icon, IconActive, IconHover).ToArray();
 
         private static UITextureAtlas.SpriteInfo[] HeaderButtons(int texWidth, int texHeight, Rect rect) => TextureHelper.GetSpritesInfo(texWidth, texHeight, rect, 25, 25, new RectOffset(4, 4, 4, 4), 2, Reset, MakeStraight).ToArray();
     }
