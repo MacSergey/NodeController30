df6a2e31f476ce701e495b8885698cd590d69ab5
MacSergey <macsergey@me.com>
2021-04-30 14:37:18 +0300

    update version

--- NodeControllerRenewal/Properties/AssemblyInfo.cs
+++ NodeControllerRenewal/Properties/AssemblyInfo.cs
@@ -12,5 +12,10 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("c7dd6e8a-e402-47f7-8b2a-52f37a7c21ef")]
 
+#if BETA
 [assembly: AssemblyVersion("3.0.0.903")]
-[assembly: AssemblyFileVersion("3.0.0.902")]
+[assembly: AssemblyFileVersion("3.0.0.903")]
+#else
+[assembly: AssemblyVersion("3.0")]
+[assembly: AssemblyFileVersion("3.0")]
+#endif
