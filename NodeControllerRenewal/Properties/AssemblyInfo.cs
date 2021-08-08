48c55ace00af8d1c32b05229263b00a07ddb44a9
MacSergey <macsergey@me.com>
2021-08-08 10:33:53 +0300

    Added Indonesian translation

--- NodeControllerRenewal/Properties/AssemblyInfo.cs
+++ NodeControllerRenewal/Properties/AssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("c7dd6e8a-e402-47f7-8b2a-52f37a7c21ef")]
 
 #if BETA
-[assembly: AssemblyVersion("3.1.0.83")]
-[assembly: AssemblyFileVersion("3.1.0.83")]
+[assembly: AssemblyVersion("3.1.0.84")]
+[assembly: AssemblyFileVersion("3.1.0.84")]
 #else
 [assembly: AssemblyVersion("3.1.2")]
 [assembly: AssemblyFileVersion("3.1.2")]
