209d879c9f62854eaa63858679825451e7f55658
MacSergey <macsergey@me.com>
2025-09-30 00:28:54 +0300

    1.20.1-f1 support

--- NodeControllerRenewal/Properties/Localize.cs
+++ NodeControllerRenewal/Properties/Localize.cs
@@ -131,6 +131,11 @@ namespace NodeController
 		public static string Mod_WhatsNewMessage3_5_2 => LocaleManager.GetString("Mod_WhatsNewMessage3_5_2", Culture);
 
 		/// <summary>
+		/// [UPDATED] Updated required game version to 1.20.1-f1
+		/// </summary>
+		public static string Mod_WhatsNewMessage3_5_3 => LocaleManager.GetString("Mod_WhatsNewMessage3_5_3", Culture);
+
+		/// <summary>
 		/// Bend
 		/// </summary>
 		public static string NodeStyle_Bend => LocaleManager.GetString("NodeStyle_Bend", Culture);
